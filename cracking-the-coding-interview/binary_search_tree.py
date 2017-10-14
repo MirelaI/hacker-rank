5eef827b1a605fb8c3f82bd1effbbae2108faf7f
MirelaI <imirela@cpan.org>
2017-10-14 21:40:45 +0100

    Add two more chanllanges: Binary Search Tree and Cycle

---
+++ cracking-the-coding-interview/binary_search_tree.py
@@ -0,0 +1,16 @@
+""" Node is defined as
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+"""
+
+def check_binary_search_tree_(root):
+    return check_order(root, 0, 10**4)
+
+def check_order(node, min, max):
+    if node is None:
+        return True
+
+    return min < node.data < max and check_order(node.left, min, node.data) and check_order(node.right, node.data, max)
